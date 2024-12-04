fbb4baaad8d84e6354aef8a00e0085a63307c70e
alexshaw98 <alexshaw98.hotmail.com>
2024-12-03 18:50:54 -0600

    Code beautification

--- 1-VPC.tf
+++ 1-VPC.tf
@@ -4,10 +4,10 @@ resource "aws_vpc" "TMMC-Tokyo" {
   cidr_block = "10.80.0.0/16"
 
   tags = {
-    Name = "TMMC-Tokyo"
+    Name    = "TMMC-Tokyo"
     Service = "J-Teledoctor"
-    Owner = "Hungry-Wolves"
-    Planet = "Earth"
+    Team    = "Hungry-Wolves"
+    Planet  = "Earth"
   }
 }
 
@@ -16,69 +16,69 @@ resource "aws_vpc" "TMMC-Hong-Kong" {
   cidr_block = "10.81.0.0/16"
 
   tags = {
-    Name = "TMMC-Hong-Kong"
+    Name    = "TMMC-Hong-Kong"
     Service = "J-Teledoctor"
-    Owner = "Hungry-Wolves"
-    Planet = "Earth"
+    Team    = "Hungry-Wolves"
+    Planet  = "Earth"
   }
 }
 
 resource "aws_vpc" "TMMC-London" {
-  provider = aws.London
-  cidr_block = "10.82.0.0/16"
+  provider    = aws.London
+  cidr_block  = "10.82.0.0/16"
 
   tags = {
-    Name = "TMMC-London"
+    Name    = "TMMC-London"
     Service = "J-Teledoctor"
-    Owner = "Hungry-Wolves"
-    Planet = "Earth"
+    Team    = "Hungry-Wolves"
+    Planet  = "Earth"
   }
 }
 
 resource "aws_vpc" "TMMC-Sao-Paulo" {
-  provider = aws.Sao-Paulo
+  provider   = aws.Sao-Paulo
   cidr_block = "10.83.0.0/16"
 
   tags = {
-    Name = "TMMC-Sao-Paulo"
+    Name    = "TMMC-Sao-Paulo"
     Service = "J-Teledoctor"
-    Owner = "Hungry-Wolves"
-    Planet = "Earth"
+    Team    = "Hungry-Wolves"
+    Planet  = "Earth"
   }
 }
 
 resource "aws_vpc" "TMMC-California" {
-  provider = aws.California
+  provider   = aws.California
   cidr_block = "10.84.0.0/16"
 
   tags = {
-    Name = "TMMC-California"
+    Name    = "TMMC-California"
     Service = "J-Teledoctor"
-    Owner = "Hungry-Wolves"
-    Planet = "Earth"
+    Team    = "Hungry-Wolves"
+    Planet  = "Earth"
   }
 }
 
 resource "aws_vpc" "TMMC-Australia" {
-  provider = aws.Australia
+  provider   = aws.Australia
   cidr_block = "10.85.0.0/16"
 
   tags = {
-    Name = "TMMC-Australia"
+    Name    = "TMMC-Australia"
     Service = "J-Teledoctor"
-    Owner = "Hungry-Wolves"
-    Planet = "Earth"
+    Team    = "Hungry-Wolves"
+    Planet  = "Earth"
   }
 }
 
 resource "aws_vpc" "TMMC-New-York" {
-  provider = aws.New-York
+  provider   = aws.New-York
   cidr_block = "10.86.0.0/16"
 
   tags = {
-    Name = "TMMC-New-York"
+    Name    = "TMMC-New-York"
     Service = "J-Teledoctor"
-    Owner = "Hungry-Wolves"
-    Planet = "Earth"
+    Team    = "Hungry-Wolves"
+    Planet  = "Earth"
   }
 }
