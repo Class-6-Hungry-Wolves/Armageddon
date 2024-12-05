cc45ccedf67a38473853c96db5a4ae10675dd77b
alexshaw98 <alexshaw98.hotmail.com>
2024-12-04 20:56:57 -0600

    updating launch template file

--- 7-launchtemplate.tf
+++ 7-launchtemplate.tf
@@ -1,69 +1,505 @@
-# resource "aws_launch_template" "app1_LT" {
-#   name_prefix   = "app1_LT"
-#   image_id      = "ami-06ed60ed1369448bd"  
-#   instance_type = "t2.micro"
+# Hong Kong Launch Template
 
-#   key_name = "MyLinuxBox"
+resource "aws_launch_template" "Hong-Kong-LT" {
+  name_prefix   = "Hong-Kong-LT"
+  image_id      = "ami-06f707739f2271995"  
+  instance_type = "t2.micro"
 
-#   vpc_security_group_ids = [aws_security_group.app1-sg01-servers.id]
+  key_name = "Key-Hong-Kong"
 
-#   user_data = base64encode(<<-EOF
-#     #!/bin/bash
-#     yum update -y
-#     yum install -y httpd
-#     systemctl start httpd
-#     systemctl enable httpd
+  vpc_security_group_ids = [aws_security_group.TMMC-Hong-Kong-sg01-servers.id]
 
-#     # Get the IMDSv2 token
-#     TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install -y httpd
+    systemctl start httpd
+    systemctl enable httpd
 
-#     # Background the curl requests
-#     curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/local-ipv4 &> /tmp/local_ipv4 &
-#     curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/placement/availability-zone &> /tmp/az &
-#     curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/ &> /tmp/macid &
-#     wait
+    # Get the IMDSv2 token
+    TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 
-#     macid=$(cat /tmp/macid)
-#     local_ipv4=$(cat /tmp/local_ipv4)
-#     az=$(cat /tmp/az)
-#     vpc=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/$macid/vpc-id)
+    # Background the curl requests
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/local-ipv4 &> /tmp/local_ipv4 &
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/placement/availability-zone &> /tmp/az &
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/ &> /tmp/macid &
+    wait
 
-#     # Create HTML file
-#     cat <<-HTML > /var/www/html/index.html
-#     <!doctype html>
-#     <html lang="en" class="h-100">
-#     <head>
-#     <title>Details for EC2 instance</title>
-#     </head>
-#     <body>
-#     <div>
-#     <h1>Malgus Clan</h1>
-#     <h1>Chains Broken in Ireland</h1>
-#     <p><b>Instance Name:</b> $(hostname -f) </p>
-#     <p><b>Instance Private Ip Address: </b> $local_ipv4</p>
-#     <p><b>Availability Zone: </b> $az</p>
-#     <p><b>Virtual Private Cloud (VPC):</b> $vpc</p>
-#     </div>
-#     </body>
-#     </html>
-#     HTML
+    macid=$(cat /tmp/macid)
+    local_ipv4=$(cat /tmp/local_ipv4)
+    az=$(cat /tmp/az)
+    vpc=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/$macid/vpc-id)
 
-#     # Clean up the temp files
-#     rm -f /tmp/local_ipv4 /tmp/az /tmp/macid
-#   EOF
-#   )
+    # Create HTML file
+    cat <<-HTML > /var/www/html/index.html
+    <!doctype html>
+    <html lang="en" class="h-100">
+    <head>
+    <title>Details Hong Kong EC2 instances</title>
+    </head>
+    <body>
+    <div>
+    <h1>Malgus Clan</h1>
+    <h1>Chains Broken in Hong Kong</h1>
+    <p><b>Instance Name:</b> $(hostname -f) </p>
+    <p><b>Instance Private Ip Address: </b> $local_ipv4</p>
+    <p><b>Availability Zone: </b> $az</p>
+    <p><b>Virtual Private Cloud (VPC):</b> $vpc</p>
+    </div>
+    </body>
+    </html>
+    HTML
 
-#   tag_specifications {
-#     resource_type = "instance"
-#     tags = {
-#       Name    = "app1_LT"
-#       Service = "application1"
-#       Owner   = "Chewbacca"
-#       Planet  = "Mustafar"
-#     }
-#   }
+    # Clean up the temp files
+    rm -f /tmp/local_ipv4 /tmp/az /tmp/macid
+  EOF
+  )
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name    		= "Hong Kong LT"
+      Service 		= "Launch Template"
+      Owner   		= "Alex"
+      Team  		= "Hungry Wolves"
+	  Assignment	= "Armageddon"
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# New York Launch Template
+
+resource "aws_launch_template" "New-York-LT" {
+  name_prefix   = "New-York-LT"
+  image_id      = "ami-0453ec754f44f9a4a"  
+  instance_type = "t2.micro"
+
+  key_name = "Key-New-York"
+
+  vpc_security_group_ids = [aws_security_group.TMMC-New-York-LT-sg01-servers.id]
+
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install -y httpd
+    systemctl start httpd
+    systemctl enable httpd
+
+    # Get the IMDSv2 token
+    TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+
+    # Background the curl requests
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/local-ipv4 &> /tmp/local_ipv4 &
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/placement/availability-zone &> /tmp/az &
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/ &> /tmp/macid &
+    wait
+
+    macid=$(cat /tmp/macid)
+    local_ipv4=$(cat /tmp/local_ipv4)
+    az=$(cat /tmp/az)
+    vpc=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/$macid/vpc-id)
+
+    # Create HTML file
+    cat <<-HTML > /var/www/html/index.html
+    <!doctype html>
+    <html lang="en" class="h-100">
+    <head>
+    <title>Details New York EC2 instances</title>
+    </head>
+    <body>
+    <div>
+    <h1>New York/h1>
+    <h1>Chains Broken in New York</h1>
+    <p><b>Instance Name:</b> $(hostname -f) </p>
+    <p><b>Instance Private Ip Address: </b> $local_ipv4</p>
+    <p><b>Availability Zone: </b> $az</p>
+    <p><b>Virtual Private Cloud (VPC):</b> $vpc</p>
+    </div>
+    </body>
+    </html>
+    HTML
+
+    # Clean up the temp files
+    rm -f /tmp/local_ipv4 /tmp/az /tmp/macid
+  EOF
+  )
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name    		= "New York LT"
+      Service 		= "Launch Template"
+      Owner   		= "Alex"
+      Team  		= "Hungry Wolves"
+	  Assignment	= "Armageddon"
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# London Launch Template
+resource "aws_launch_template" "London-LT" {
+  name_prefix   = "London-LT"
+  image_id      = "ami-0c76bd4bd302b30ec"  
+  instance_type = "t2.micro"
+
+  key_name = "Key-London"
+
+  vpc_security_group_ids = [aws_security_group.TMMC-London-LT-sg01-servers.id]
+
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install -y httpd
+    systemctl start httpd
+    systemctl enable httpd
+
+    # Get the IMDSv2 token
+    TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+
+    # Background the curl requests
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/local-ipv4 &> /tmp/local_ipv4 &
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/placement/availability-zone &> /tmp/az &
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/ &> /tmp/macid &
+    wait
+
+    macid=$(cat /tmp/macid)
+    local_ipv4=$(cat /tmp/local_ipv4)
+    az=$(cat /tmp/az)
+    vpc=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/$macid/vpc-id)
+
+    # Create HTML file
+    cat <<-HTML > /var/www/html/index.html
+    <!doctype html>
+    <html lang="en" class="h-100">
+    <head>
+    <title>Details London EC2 instances</title>
+    </head>
+    <body>
+    <div>
+    <h1>London/h1>
+    <h1>Chains Broken in London</h1>
+    <p><b>Instance Name:</b> $(hostname -f) </p>
+    <p><b>Instance Private Ip Address: </b> $local_ipv4</p>
+    <p><b>Availability Zone: </b> $az</p>
+    <p><b>Virtual Private Cloud (VPC):</b> $vpc</p>
+    </div>
+    </body>
+    </html>
+    HTML
+
+    # Clean up the temp files
+    rm -f /tmp/local_ipv4 /tmp/az /tmp/macid
+  EOF
+  )
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name    		= "London LT"
+      Service 		= "Launch Template"
+      Owner   		= "Alex"
+      Team  		= "Hungry Wolves"
+	  Assignment	= "Armageddon"
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Australia Launch Template
+resource "aws_launch_template" "Australia-LT" {
+  name_prefix   = "Australia-LT"
+  image_id      = "ami-0146fc9ad419e2cfd"  
+  instance_type = "t2.micro"
+
+  key_name = "Key-Australia"
+
+  vpc_security_group_ids = [aws_security_group.TMMC-Australia-LT-sg01-servers.id]
+
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install -y httpd
+    systemctl start httpd
+    systemctl enable httpd
+
+    # Get the IMDSv2 token
+    TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+
+    # Background the curl requests
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/local-ipv4 &> /tmp/local_ipv4 &
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/placement/availability-zone &> /tmp/az &
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/ &> /tmp/macid &
+    wait
+
+    macid=$(cat /tmp/macid)
+    local_ipv4=$(cat /tmp/local_ipv4)
+    az=$(cat /tmp/az)
+    vpc=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/$macid/vpc-id)
+
+    # Create HTML file
+    cat <<-HTML > /var/www/html/index.html
+    <!doctype html>
+    <html lang="en" class="h-100">
+    <head>
+    <title>Details Australia EC2 instances</title>
+    </head>
+    <body>
+    <div>
+    <h1>Australia/h1>
+    <h1>Chains Broken in Australia</h1>
+    <p><b>Instance Name:</b> $(hostname -f) </p>
+    <p><b>Instance Private Ip Address: </b> $local_ipv4</p>
+    <p><b>Availability Zone: </b> $az</p>
+    <p><b>Virtual Private Cloud (VPC):</b> $vpc</p>
+    </div>
+    </body>
+    </html>
+    HTML
+
+    # Clean up the temp files
+    rm -f /tmp/local_ipv4 /tmp/az /tmp/macid
+  EOF
+  )
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name    		= "Australia-LT"
+      Service 		= "Launch Template"
+      Owner   		= "Alex"
+      Team  		= "Hungry Wolves"
+	  Assignment	= "Armageddon"
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# California Launch Template
+resource "aws_launch_template" "California-LT" {
+  name_prefix   = "California-LT"
+  image_id      = "ami-038bba9a164eb3dc1"  
+  instance_type = "t2.micro"
+
+  key_name = "Key-California"
+
+  vpc_security_group_ids = [aws_security_group.TMMC-California-LT-sg01-servers.id]
+
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install -y httpd
+    systemctl start httpd
+    systemctl enable httpd
+
+    # Get the IMDSv2 token
+    TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+
+    # Background the curl requests
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/local-ipv4 &> /tmp/local_ipv4 &
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/placement/availability-zone &> /tmp/az &
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/ &> /tmp/macid &
+    wait
+
+    macid=$(cat /tmp/macid)
+    local_ipv4=$(cat /tmp/local_ipv4)
+    az=$(cat /tmp/az)
+    vpc=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/$macid/vpc-id)
+
+    # Create HTML file
+    cat <<-HTML > /var/www/html/index.html
+    <!doctype html>
+    <html lang="en" class="h-100">
+    <head>
+    <title>Details California EC2 instances</title>
+    </head>
+    <body>
+    <div>
+    <h1>California/h1>
+    <h1>Chains Broken in California</h1>
+    <p><b>Instance Name:</b> $(hostname -f) </p>
+    <p><b>Instance Private Ip Address: </b> $local_ipv4</p>
+    <p><b>Availability Zone: </b> $az</p>
+    <p><b>Virtual Private Cloud (VPC):</b> $vpc</p>
+    </div>
+    </body>
+    </html>
+    HTML
+
+    # Clean up the temp files
+    rm -f /tmp/local_ipv4 /tmp/az /tmp/macid
+  EOF
+  )
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name    		= "California LT"
+      Service 		= "Launch Template"
+      Owner   		= "Alex"
+      Team  		= "Hungry Wolves"
+	  Assignment	= "Armageddon"
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Sao Paulo Launch Template
+resource "aws_launch_template" "Sao-Paulo-LT" {
+  name_prefix   = "Sao-Paulo-LT"
+  image_id      = "ami-0c820c196a818d66a"  
+  instance_type = "t2.micro"
+
+  key_name = "Key-Sao Paulo"
+
+  vpc_security_group_ids = [aws_security_group.TMMC-Sao-Paulo-LT-sg01-servers.id]
+
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install -y httpd
+    systemctl start httpd
+    systemctl enable httpd
+
+    # Get the IMDSv2 token
+    TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+
+    # Background the curl requests
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/local-ipv4 &> /tmp/local_ipv4 &
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/placement/availability-zone &> /tmp/az &
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/ &> /tmp/macid &
+    wait
+
+    macid=$(cat /tmp/macid)
+    local_ipv4=$(cat /tmp/local_ipv4)
+    az=$(cat /tmp/az)
+    vpc=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/$macid/vpc-id)
+
+    # Create HTML file
+    cat <<-HTML > /var/www/html/index.html
+    <!doctype html>
+    <html lang="en" class="h-100">
+    <head>
+    <title>Details Sao Paulo EC2 instances</title>
+    </head>
+    <body>
+    <div>
+    <h1>Sao Paulo/h1>
+    <h1>Chains Broken in Sao Paulo</h1>
+    <p><b>Instance Name:</b> $(hostname -f) </p>
+    <p><b>Instance Private Ip Address: </b> $local_ipv4</p>
+    <p><b>Availability Zone: </b> $az</p>
+    <p><b>Virtual Private Cloud (VPC):</b> $vpc</p>
+    </div>
+    </body>
+    </html>
+    HTML
+
+    # Clean up the temp files
+    rm -f /tmp/local_ipv4 /tmp/az /tmp/macid
+  EOF
+  )
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name    		= "Sao Paulo LT"
+      Service 		= "Launch Template"
+      Owner   		= "Alex"
+      Team  		= "Hungry Wolves"
+	  Assignment	= "Armageddon"
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Tokyo Launch Template
+resource "aws_launch_template" "Tokyo-LT" {
+  name_prefix   = "Tokyo-LT"
+  image_id      = "ami-0e577819b2a6cc996"  
+  instance_type = "t2.micro"
+
+  key_name = "Key-Tokyo"
+
+  vpc_security_group_ids = [aws_security_group.TMMC-Tokyo-LT-sg01-servers.id]
+
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install -y httpd
+    systemctl start httpd
+    systemctl enable httpd
+
+    # Get the IMDSv2 token
+    TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+
+    # Background the curl requests
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/local-ipv4 &> /tmp/local_ipv4 &
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/placement/availability-zone &> /tmp/az &
+    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/ &> /tmp/macid &
+    wait
+
+    macid=$(cat /tmp/macid)
+    local_ipv4=$(cat /tmp/local_ipv4)
+    az=$(cat /tmp/az)
+    vpc=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/$macid/vpc-id)
+
+    # Create HTML file
+    cat <<-HTML > /var/www/html/index.html
+    <!doctype html>
+    <html lang="en" class="h-100">
+    <head>
+    <title>Details Tokyo EC2 instances</title>
+    </head>
+    <body>
+    <div>
+    <h1>Tokyo/h1>
+    <h1>Chains Broken in Tokyo</h1>
+    <p><b>Instance Name:</b> $(hostname -f) </p>
+    <p><b>Instance Private Ip Address: </b> $local_ipv4</p>
+    <p><b>Availability Zone: </b> $az</p>
+    <p><b>Virtual Private Cloud (VPC):</b> $vpc</p>
+    </div>
+    </body>
+    </html>
+    HTML
+
+    # Clean up the temp files
+    rm -f /tmp/local_ipv4 /tmp/az /tmp/macid
+  EOF
+  )
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name    		= "Tokyo LT"
+      Service 		= "Launch Template"
+      Owner   		= "Alex"
+      Team  		= "Hungry Wolves"
+	  Assignment	= "Armageddon"
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
