63b5397c1e341dd84a3b8fb23cecaeb310ed5bdf
RakeemRussell <russelltechwilliamson@gmail.com>
2024-12-06 16:48:44 -0500

    updated IGW, and Nat

--- 3-IGW.tf
+++ 3-IGW.tf
@@ -1,10 +1,99 @@
-# resource "aws_internet_gateway" "igw" {
-#   vpc_id = aws_vpc.app1.id
 
-#   tags = {
-#     Name    = "app1_IG"
-#     Service = "application1"
-#     Owner   = "Luke"
-#     Planet  = "Musafar"
-#   }
-# }
+
+resource "aws_internet_gateway" "Tokyo-IG" {
+
+  provider = aws.Tokyo
+
+  vpc_id = aws_vpc.TMMC-Tokyo.id
+
+  tags = {
+    Name    = "TMMC-Tokyo"
+    Service = "J-Teledoctor"
+    Owner   = "Hungry-Wolves"
+    Planet  = "Earth"
+  }
+}
+
+resource "aws_internet_gateway" "Hong-Kong-IG" {
+
+  provider = aws.Hong-Kong
+
+  vpc_id = aws_vpc.TMMC-Hong-Kong.id
+
+  tags = {
+    Name    = "TMMC-Hong-Kong"
+    Service = "J-Teledoctor"
+    Owner   = "Hungry-Wolves"
+    Planet  = "Earth"
+  }
+}
+
+resource "aws_internet_gateway" "London-IG" {
+
+  provider = aws.London
+
+  vpc_id = aws_vpc.TMMC-London.id
+
+  tags = {
+    Name    = "TMMC-London"
+    Service = "J-Teledoctor"
+    Owner   = "Hungry-Wolves"
+    Planet  = "Earth"
+  }
+}
+
+resource "aws_internet_gateway" "Sao-Paulo-IG" {
+
+  provider = aws.Sao-Paulo
+
+  vpc_id = aws_vpc.TMMC-Sao-Paulo.id
+
+  tags = {
+    Name    = "TMMC-Sao-Paulo"
+    Service = "J-Teledoctor"
+    Owner   = "Hungry-Wolves"
+    Planet  = "Earth"
+  }
+}
+
+resource "aws_internet_gateway" "California-IG" {
+
+  provider = aws.California
+
+  vpc_id = aws_vpc.TMMC-California.id
+
+  tags = {
+    Name    = "TMMC-California"
+    Service = "J-Teledoctor"
+    Owner   = "Hungry-Wolves"
+    Planet  = "Earth"
+  }
+}
+
+resource "aws_internet_gateway" "Australia-IG" {
+
+  provider = aws.Australia
+
+  vpc_id = aws_vpc.TMMC-Australia.id
+
+  tags = {
+    Name    = "TMMC-Australia"
+    Service = "J-Teledoctor"
+    Owner   = "Hungry-Wolves"
+    Planet  = "Earth"
+  }
+}
+
+resource "aws_internet_gateway" "New-York-IG" {
+
+  provider = aws.New-York
+
+  vpc_id = aws_vpc.TMMC-New-York.id
+
+  tags = {
+    Name    = "TMMC-New-York"
+    Service = "J-Teledoctor"
+    Owner   = "Hungry-Wolves"
+    Planet  = "Earth"
+  }
+}
--- 4-NAT.tf
+++ 4-NAT.tf
@@ -1,195 +1,195 @@
-# # Hong Kong NAT
-# resource "aws_eip" "Hong-Kong-NAT" {
-#   vpc = true
+# Hong Kong NAT
+resource "aws_eip" "Hong-Kong-NAT" {
+  vpc = true
 
-#   tags = {
-#     Name        = "Hong-Kong-NAT"
-#     Service     = "Hong-Kong"
-#     Owner       = "alex"
-#     Team        = "HungryWolves"
-#     Assignment  = "Armageddon"
-#   }
-# }
+  tags = {
+    Name        = "Hong-Kong-NAT"
+    Service     = "Hong-Kong"
+    Owner       = "alex"
+    Team        = "HungryWolves"
+    Assignment  = "Armageddon"
+  }
+}
 
-# resource "aws_nat_gateway" "Hong-Kong-NAT" {
-#   allocation_id = aws_eip.Hong-Kong-NAT.id
-#   subnet_id     = aws_subnet.public-ap-east-1a.id
+resource "aws_nat_gateway" "Hong-Kong-NAT" {
+  allocation_id = aws_eip.Hong-Kong-NAT.id
+  subnet_id     = aws_subnet.public-ap-east-1a.id
 
-#   tags = {
-#     Name        = "Hong-Kong-NAT"
-#     Service     = "Hong-Kong"
-#     Owner       = "alex"
-#     Team        = "HungryWolves"
-#     Assignment  = "Armageddon"
-#   }
+  tags = {
+    Name        = "Hong-Kong-NAT"
+    Service     = "Hong-Kong"
+    Owner       = "alex"
+    Team        = "HungryWolves"
+    Assignment  = "Armageddon"
+  }
 
-#   depends_on = [aws_internet_gateway.Hong-Kong-IG]
-# }
+  depends_on = [aws_internet_gateway.Hong-Kong-IG]
+}
 
-# # Sao Paulo NAT
-# resource "aws_eip" "Sao-Paulo-NAT" {
-#   vpc = true
+# Sao Paulo NAT
+resource "aws_eip" "Sao-Paulo-NAT" {
+  vpc = true
 
-#   tags = {
-#     Name        = "Sao-Paulo-NAT"
-#     Service     = "HongKong"
-#     Owner       = "alex"
-#     Team        = "HungryWolves"
-#     Assignment  = "Armageddon"
-#   }
-# }
+  tags = {
+    Name        = "Sao-Paulo-NAT"
+    Service     = "HongKong"
+    Owner       = "alex"
+    Team        = "HungryWolves"
+    Assignment  = "Armageddon"
+  }
+}
 
-# resource "aws_nat_gateway" "Sao-Paulo-NAT" {
-#   allocation_id = aws_eip.Sao-Paulo-NAT.id
-#   subnet_id     = aws_subnet.public-sa-east-1a.id
+resource "aws_nat_gateway" "Sao-Paulo-NAT" {
+  allocation_id = aws_eip.Sao-Paulo-NAT.id
+  subnet_id     = aws_subnet.public-sa-east-1a.id
 
-#   tags = {
-#     Name        = "Sao-Paulo-NAT"
-#     Service     = "Sao-Paulo"
-#     Owner       = "alex"
-#     Team        = "HungryWolves"
-#     Assignment  = "Armageddon"
-#   }
+  tags = {
+    Name        = "Sao-Paulo-NAT"
+    Service     = "Sao-Paulo"
+    Owner       = "alex"
+    Team        = "HungryWolves"
+    Assignment  = "Armageddon"
+  }
 
-#   depends_on = [aws_internet_gateway.Sao-Paulo-IG]
-# }
+  depends_on = [aws_internet_gateway.Sao-Paulo-IG]
+}
 
-# # New York NAT
-# resource "aws_eip" "New-York-NAT" {
-#   vpc = true
+# New York NAT
+resource "aws_eip" "New-York-NAT" {
+  vpc = true
 
-#   tags = {
-#     Name        = "New-York-NAT"
-#     Service     = "New-York"
-#     Owner       = "alex"
-#     Team        = "HungryWolves"
-#     Assignment  = "Armageddon"
-#   }
-# }
+  tags = {
+    Name        = "New-York-NAT"
+    Service     = "New-York"
+    Owner       = "alex"
+    Team        = "HungryWolves"
+    Assignment  = "Armageddon"
+  }
+}
 
-# resource "aws_nat_gateway" "New-York-NAT" {
-#   allocation_id = aws_eip.New-York-NAT.id
-#   subnet_id     = aws_subnet.public-us-east-1a.id
+resource "aws_nat_gateway" "New-York-NAT" {
+  allocation_id = aws_eip.New-York-NAT.id
+  subnet_id     = aws_subnet.public-us-east-1a.id
 
-#   tags = {
-#     Name        = "New-York-NAT"
-#     Service     = "New-York"
-#     Owner       = "alex"
-#     Team        = "HungryWolves"
-#     Assignment  = "Armageddon"
-#   }
+  tags = {
+    Name        = "New-York-NAT"
+    Service     = "New-York"
+    Owner       = "alex"
+    Team        = "HungryWolves"
+    Assignment  = "Armageddon"
+  }
 
-#   depends_on = [aws_internet_gateway.New-York-IG]
-# }
+  depends_on = [aws_internet_gateway.New-York-IG]
+}
 
-# # London NAT
-# resource "aws_eip" "London-NAT" {
-#   vpc = true
+# London NAT
+resource "aws_eip" "London-NAT" {
+  vpc = true
 
-#   tags = {
-#     Name        = "London-NAT"
-#     Service     = "London"
-#     Owner       = "alex"
-#     Team        = "HungryWolves"
-#     Assignment  = "Armageddon"
-#   }
-# }
+  tags = {
+    Name        = "London-NAT"
+    Service     = "London"
+    Owner       = "alex"
+    Team        = "HungryWolves"
+    Assignment  = "Armageddon"
+  }
+}
 
-# resource "aws_nat_gateway" "London-NAT" {
-#   allocation_id = aws_eip.London-NAT.id
-#   subnet_id     = aws_subnet.public-eu-west-2a.id
+resource "aws_nat_gateway" "London-NAT" {
+  allocation_id = aws_eip.London-NAT.id
+  subnet_id     = aws_subnet.public-eu-west-2a.id
 
-#   tags = {
-#     Name        = "London-NAT"
-#     Service     = "London"
-#     Owner       = "alex"
-#     Team        = "HungryWolves"
-#     Assignment  = "Armageddon"
-#   }
+  tags = {
+    Name        = "London-NAT"
+    Service     = "London"
+    Owner       = "alex"
+    Team        = "HungryWolves"
+    Assignment  = "Armageddon"
+  }
 
-#   depends_on = [aws_internet_gateway.London-IG]
-# }
+  depends_on = [aws_internet_gateway.London-IG]
+}
 
-# # Australia NAT
-# resource "aws_eip" "Australia-NAT" {
-#   vpc = true
+# Australia NAT
+resource "aws_eip" "Australia-NAT" {
+  vpc = true
 
-#   tags = {
-#     Name        = "Australia-NAT"
-#     Service     = "Australia"
-#     Owner       = "alex"
-#     Team        = "HungryWolves"
-#     Assignment  = "Armageddon"
-#   }
-# }
+  tags = {
+    Name        = "Australia-NAT"
+    Service     = "Australia"
+    Owner       = "alex"
+    Team        = "HungryWolves"
+    Assignment  = "Armageddon"
+  }
+}
 
-# resource "aws_nat_gateway" "Australia-NAT" {
-#   allocation_id = aws_eip.Australia-NAT.id
-#   subnet_id     = aws_subnet.ap-southeast-2a.id
+resource "aws_nat_gateway" "Australia-NAT" {
+  allocation_id = aws_eip.Australia-NAT.id
+  subnet_id     = aws_subnet.public-ap-southeast-2a.id
 
-#   tags = {
-#     Name        = "Australia-NAT"
-#     Service     = "Australia"
-#     Owner       = "alex"
-#     Team        = "HungryWolves"
-#     Assignment  = "Armageddon"
-#   }
+  tags = {
+    Name        = "Australia-NAT"
+    Service     = "Australia"
+    Owner       = "alex"
+    Team        = "HungryWolves"
+    Assignment  = "Armageddon"
+  }
 
-#   depends_on = [aws_internet_gateway.Australia-IG]
-# }
+  depends_on = [aws_internet_gateway.Australia-IG]
+}
 
-# # California NAT
-# resource "aws_eip" "California-NAT" {
-#   vpc = true
+# California NAT
+resource "aws_eip" "California-NAT" {
+  vpc = true
 
-#   tags = {
-#     Name        = "California-NAT"
-#     Service     = "California"
-#     Owner       = "alex"
-#     Team        = "HungryWolves"
-#     Assignment  = "Armageddon"
-#   }
-# }
+  tags = {
+    Name        = "California-NAT"
+    Service     = "California"
+    Owner       = "alex"
+    Team        = "HungryWolves"
+    Assignment  = "Armageddon"
+  }
+}
 
-# resource "aws_nat_gateway" "California-NAT" {
-#   allocation_id = aws_eip.California-NAT.id
-#   subnet_id     = aws_subnet.us-west-1a.id
+resource "aws_nat_gateway" "California-NAT" {
+  allocation_id = aws_eip.California-NAT.id
+  subnet_id     = aws_subnet.public-us-west-1a.id
 
-#   tags = {
-#     Name        = "California-NAT"
-#     Service     = "California"
-#     Owner       = "alex"
-#     Team        = "HungryWolves"
-#     Assignment  = "Armageddon"
-#   }
+  tags = {
+    Name        = "California-NAT"
+    Service     = "California"
+    Owner       = "alex"
+    Team        = "HungryWolves"
+    Assignment  = "Armageddon"
+  }
 
-#   depends_on = [aws_internet_gateway.California-IG]
-# }
+  depends_on = [aws_internet_gateway.California-IG]
+}
 
-# # Tokyo NAT
-# resource "aws_eip" "Tokyo-NAT" {
-#   vpc = true
+# Tokyo NAT
+resource "aws_eip" "Tokyo-NAT" {
+  vpc = true
 
-#   tags = {
-#     Name        = "Tokyo-NAT"
-#     Service     = "Tokyo"
-#     Owner       = "alex"
-#     Team        = "HungryWolves"
-#     Assignment  = "Armageddon"
-#   }
-# }
+  tags = {
+    Name        = "Tokyo-NAT"
+    Service     = "Tokyo"
+    Owner       = "alex"
+    Team        = "HungryWolves"
+    Assignment  = "Armageddon"
+  }
+}
 
-# resource "aws_nat_gateway" "Tokyo-NAT" {
-#   allocation_id = aws_eip.Tokyo-NAT.id
-#   subnet_id     = aws_subnet.public-ap-northeast-1a.id
+resource "aws_nat_gateway" "Tokyo-NAT" {
+  allocation_id = aws_eip.Tokyo-NAT.id
+  subnet_id     = aws_subnet.public-ap-northeast-1a.id
 
-#   tags = {
-#     Name        = "Tokyo-NAT"
-#     Service     = "Tokyo"
-#     Owner       = "alex"
-#     Team        = "HungryWolves"
-#     Assignment  = "Armageddon"
-#   }
+  tags = {
+    Name        = "Tokyo-NAT"
+    Service     = "Tokyo"
+    Owner       = "alex"
+    Team        = "HungryWolves"
+    Assignment  = "Armageddon"
+  }
 
-#   depends_on = [aws_internet_gateway.Tokyo-IG]
-# }
+  depends_on = [aws_internet_gateway.Tokyo-IG]
+}
