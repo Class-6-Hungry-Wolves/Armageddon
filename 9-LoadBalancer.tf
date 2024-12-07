7ecc2689fe60ebb8d62dc48c15dfe9a4b0d52646
vbcloud <vbru04@gmail.com>
2024-12-07 14:47:55 -0500

    9-LoadBalancer

--- 9-LoadBalancer.tf
+++ 9-LoadBalancer.tf
@@ -1,123 +1,118 @@
-# Regions List
-#locals {
-  tokyo_region      = "ap-northeast-1"
-  newyork_region    = "us-east-1"
-  london_region     = "eu-west-2"
-  saopaulo_region   = "sa-east-1"
-  australia_region  = "ap-southeast-2"
-  hongkong_region   = "ap-east-1"
-  california_region = "us-west-1"
-#}
+locals {
+  regions = {
+    tokyo = {
+      name = "ap-northeast-1"
+      sg_id = aws_security_group.tokyo_sg.id
+      subnets = [aws_subnet.tokyo_public_subnet_1.id, aws_subnet.tokyo_public_subnet_2.id]
+    }
+    newyork = {
+      name = "us-east-1"
+      sg_id = aws_security_group.newyork_sg.id
+      subnets = [aws_subnet.newyork_public_subnet_1.id, aws_subnet.newyork_public_subnet_2.id]
+    }
+    london = {
+      name = "eu-west-2"
+      sg_id = aws_security_group.london_sg.id
+      subnets = [aws_subnet.london_public_subnet_1.id, aws_subnet.london_public_subnet_2.id]
+    }
+    saopaulo = {
+      name = "sa-east-1"
+      sg_id = aws_security_group.saopaulo_sg.id
+      subnets = [aws_subnet.saopaulo_public_subnet_1.id, aws_subnet.saopaulo_public_subnet_2.id]
+    }
+    australia = {
+      name = "ap-southeast-2"
+      sg_id = aws_security_group.australia_sg.id
+      subnets = [aws_subnet.australia_public_subnet_1.id, aws_subnet.australia_public_subnet_2.id]
+    }
+    hongkong = {
+      name = "ap-east-1"
+      sg_id = aws_security_group.hongkong_sg.id
+      subnets = [aws_subnet.hongkong_public_subnet_1.id, aws_subnet.hongkong_public_subnet_2.id]
+    }
+    california = {
+      name = "us-west-1"
+      sg_id = aws_security_group.california_sg.id
+      subnets = [aws_subnet.california_public_subnet_1.id, aws_subnet.california_public_subnet_2.id]
+    }
+  }
+}
 
-# Load Balancers for Each Region
-resource "aws_lb" "tokyo_alb" {
-  name               = "tmmc-telehealth-alb-tokyo"
+resource "aws_lb" "telehealth_alb" {
+  for_each = local.regions
+
+  name               = "tmmc-telehealth-alb-${each.key}"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.tokyo_sg.id]
-  subnets            = [aws_subnet.tokyo_public_subnet_1.id, aws_subnet.tokyo_public_subnet_2.id]
+  security_groups    = [each.value.sg_id]
+  subnets            = each.value.subnets
 
   enable_deletion_protection = true
 
   tags = {
     Name        = "TMMC Telehealth ALB"
-    Region      = "tokyo"
+    Region      = each.key
     Environment = "Production"
   }
 }
 
-resource "aws_lb" "newyork_alb" {
-  name               = "tmmc-telehealth-alb-newyork"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.newyork_sg.id]
-  subnets            = [aws_subnet.newyork_public_subnet_1.id, aws_subnet.newyork_public_subnet_2.id]
+# HTTPS Listener
+resource "aws_lb_listener" "telehealth_https_listener" {
+  for_each = local.regions
 
-  enable_deletion_protection = true
+  load_balancer_arn = aws_lb.telehealth_alb[each.key].arn
+  port              = 443
+  protocol          = "HTTPS"
 
-  tags = {
-    Name        = "TMMC Telehealth ALB"
-    Region      = "newyork"
-    Environment = "Production"
+  ssl_policy      = "ELBSecurityPolicy-2016-08"
+  certificate_arn = var.ssl_certificate_arn  # You'll need to define this variable
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.telehealth_tg[each.key].arn
   }
 }
 
-resource "aws_lb" "london_alb" {
-  name               = "tmmc-telehealth-alb-london"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.london_sg.id]
-  subnets            = [aws_subnet.london_public_subnet_1.id, aws_subnet.london_public_subnet_2.id]
+# HTTP Listener (Redirect to HTTPS)
+resource "aws_lb_listener" "telehealth_http_listener" {
+  for_each = local.regions
 
-  enable_deletion_protection = true
+  load_balancer_arn = aws_lb.telehealth_alb[each.key].arn
+  port              = 80
+  protocol          = "HTTP"
 
-  tags = {
-    Name        = "TMMC Telehealth ALB"
-    Region      = "london"
-    Environment = "Production"
+  default_action {
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
   }
 }
 
-resource "aws_lb" "saopaulo_alb" {
-  name               = "tmmc-telehealth-alb-saopaulo"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.saopaulo_sg.id]
-  subnets            = [aws_subnet.saopaulo_public_subnet_1.id, aws_subnet.saopaulo_public_subnet_2.id]
+# Target Group
+resource "aws_lb_target_group" "telehealth_tg" {
+  for_each = local.regions
 
-  enable_deletion_protection = true
+  name     = "tmmc-telehealth-tg-${each.key}"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc[each.key].id  # Assumes you have a VPC resource for each region
 
-  tags = {
-    Name        = "TMMC Telehealth ALB"
-    Region      = "saopaulo"
-    Environment = "Production"
+  health_check {
+    enabled             = true
+    path                = "/health"
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+    timeout             = 5
+    interval            = 30
+    matcher             = "200"
   }
-}
-
-resource "aws_lb" "australia_alb" {
-  name               = "tmmc-telehealth-alb-australia"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.australia_sg.id]
-  subnets            = [aws_subnet.australia_public_subnet_1.id, aws_subnet.australia_public_subnet_2.id]
-
-  enable_deletion_protection = true
 
   tags = {
-    Name        = "TMMC Telehealth ALB"
-    Region      = "australia"
-    Environment = "Production"
-  }
-}
-
-resource "aws_lb" "hongkong_alb" {
-  name               = "tmmc-telehealth-alb-hongkong"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.hongkong_sg.id]
-  subnets            = [aws_subnet.hongkong_public_subnet_1.id, aws_subnet.hongkong_public_subnet_2.id]
-
-  enable_deletion_protection = true
-
-  tags = {
-    Name        = "TMMC Telehealth ALB"
-    Region      = "hongkong"
-    Environment = "Production"
-  }
-}
-
-resource "aws_lb" "california_alb" {
-  name               = "tmmc-telehealth-alb-california"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.california_sg.id]
-  subnets            = [aws_subnet.california_public_subnet_1.id, aws_subnet.california_public_subnet_2.id]
-
-  enable_deletion_protection = true
-
-  tags = {
-    Name        = "TMMC Telehealth ALB"
-    Region      = "california"
+    Name        = "TMMC Telehealth Target Group"
+    Region      = each.key
     Environment = "Production"
   }
 }
