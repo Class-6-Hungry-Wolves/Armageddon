2b07b3b7781b45d49e5be95cdd68f5d5c1c153b2
Jourdan Callahan <jourdancallahan8@gmail.com>
2025-01-01 18:34:01 -0500

    added syslog security group

--- 6-SG01-All.tf
+++ 6-SG01-All.tf
@@ -537,3 +537,48 @@ resource "aws_security_group" "TMMC-Sao-Paulo-sg02-LB" {
 
 
 }
+
+resource "aws_security_group" "TMMC-Toyko-syslog-server" {
+  name        = "TMMC-Toyko-syslog-server"
+  provider    = aws.Tokyo
+  description = "TMMC-Toyko-syslog-server"
+  vpc_id      = aws_vpc.TMMC-Tokyo.id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "TMMC-Tokyo-syslog-server"
+    Service = "J-Teledoctor"
+    Owner   = "Hungry-Wolves"
+    Planet  = "Earth"
+  }
+
+}
