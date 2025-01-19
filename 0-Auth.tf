0a0f71789096ecf9f4c172b1caa5dbd6d3ececb3
Jourdan Callahan <jourdancallahan8@gmail.com>
2025-01-19 13:35:53 -0500

    Use our HCP Terraform organization

--- 0-Auth.tf
+++ 0-Auth.tf
@@ -56,11 +56,4 @@ provider "aws" {
   region = "us-east-1"
   alias  = "New-York"
 }
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
+
