7926f645ade939ffd28a1ae57e5720c75d0a801c
Jourdan Callahan <jourdancallahan8@gmail.com>
2025-01-19 13:13:46 -0500

    Use our HCP Terraform organization

--- 0-Auth.tf
+++ 0-Auth.tf
@@ -1,5 +1,28 @@
 # These are the providers and the regions we are using to expand Tokyo Midtown Medical Center's 
 # services worldwide 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = ">= 1.1.0"
+
+  cloud { 
+    
+    organization = "Hungry-Wolves" 
+
+    workspaces { 
+      name = "Armageddon" 
+    } 
+  } 
+}
+
 provider "aws" {
   region = "ap-northeast-1"
   alias = "Tokyo"
